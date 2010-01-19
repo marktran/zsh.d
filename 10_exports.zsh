904f0e69e2732321a7c403eae348ffdb92627ac4
Mark Tran <mark@nirv.net>
2010-01-18 18:51:37 -0800

    removed $PYTHONPATH export

--- 10_exports.zsh
+++ 10_exports.zsh
@@ -6,7 +6,6 @@ export HOST=$(hostname)
 export HOST=${HOST%%.*}
 export LESSOPEN="|lesspipe.sh %s"
 export PAGER=less
-export PYTHONPATH=$HOME/lib/python
 export PYTHONSTARTUP=$HOME/.pythonrc
 export READNULLCMD=cat
 export SSH_ENV=$HOME/.ssh/environment

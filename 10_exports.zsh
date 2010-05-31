5258000588c8c90af2e6d5540e6d1e95896e91bb
Mark Tran <mark@nirv.net>
2010-05-31 04:10:54 -0700

    add custom colored ls for GNU ls

--- 10_exports.zsh
+++ 10_exports.zsh
@@ -5,6 +5,7 @@ export EMAIL='Mark Tran <mark@nirv.net>'
 export HOST=$(hostname)
 export HOST=${HOST%%.*}
 export LESSOPEN="|lesspipe.sh %s"
+export LS_COLORS='di=32:fi=0:ln=35:pi=5:so=5:bd=5:cd=5:or=31'
 export LSCOLORS="cxfxcxdxbxegedabagacad"
 export PAGER=less
 export PYTHONSTARTUP=$HOME/.pythonrc

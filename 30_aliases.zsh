fce38c1c4c099bcace2cc360e39223ad83d86ed7
Mark Tran <mark.tran@gmail.com>
2010-11-17 14:08:13 -0800

    new aliases from oh-my-zsh

--- 30_aliases.zsh
+++ 30_aliases.zsh
@@ -1,19 +1,36 @@
 # .zsh.d/30_aliases.zsh : Mark Tran <mark@nirv.net>
 
-alias ^L=clear
+alias _='sudo'
+alias -- -='cd -'
+alias ^L='clear'
+alias a="$(whence ack-grep || whence ack) --nocolor"
 alias ack="$(whence ack-grep || whence ack) --nocolor"
 alias awk="$(whence gawk || whence awk)"
-alias m=mplayer
-alias rm="rm -i"
+alias d='dirs -v'
+alias history='fc -l 1'
+alias l='ls'
+alias m='mplayer'
+alias pu='pushd'
+alias po='popd'
+alias rm='rm -i'
 alias sfv="cksfv -g *.sfv"
 alias sourcerc=". ~/.zsh.d/.zshrc"
-alias startx="startx 2>&1|tee ~/var/log/startx.log"
 alias xargs="$(whence gxargs || whence xargs)"
 
 alias -g A="| ack"
 alias -g L="| less"
 alias -g X="| xargs -i"
 alias -g XR="| xargs -i unrar x {}"
+
+alias 1='cd -'
+alias 2='cd +2'
+alias 3='cd +3'
+alias 4='cd +4'
+alias 5='cd +5'
+alias 6='cd +6'
+alias 7='cd +7'
+alias 8='cd +8'
+alias 9='cd +9'
 
 # radio streams
 alias bbc="mplayer -cache-min 4 -playlist \

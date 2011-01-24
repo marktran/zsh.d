fc653f018ed3bb547d26db25d8eaf4f4a5e58eac
Mark Tran <mark.tran@gmail.com>
2011-01-23 19:52:17 -0800

    quota that shit

--- 10_exports.zsh
+++ 10_exports.zsh
@@ -1,13 +1,14 @@
 # .zsh.d/10_exports.zsh : Mark Tran <mark@nirv.net>
 
 export EMAIL='Mark Tran <mark.tran@gmail.com>'
-export HOST=$(hostname)
-export HOST=${HOST%%.*}
+export HOST="$(hostname)"
+export HOST="${HOST%%.*}"
 export LESSOPEN="|lesspipe.sh %s"
 export LS_COLORS='di=32:fi=0:ln=35:pi=5:so=5:bd=5:cd=5:or=31'
 export LSCOLORS="cxfxcxdxbxegedabagacad"
-export PAGER=less
-export PYTHONSTARTUP=$HOME/.pythonrc
-export READNULLCMD=cat
-export SSH_ENV=$HOME/.ssh/environment
-export UNAME=$(uname)
+export NODE_PATH='/usr/local/lib/node'
+export PAGER='less'
+export PYTHONSTARTUP="$HOME/.pythonrc"
+export READNULLCMD='cat'
+export SSH_ENV="$HOME/.ssh/environment"
+export UNAME="$(uname)"

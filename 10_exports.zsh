0a638394f02e5c907fade9ccb5c48eb94632fa58
Mark Tran <mark.tran@gmail.com>
2011-05-19 18:00:34 -0700

    virtualenv/pip environment settings

--- 10_exports.zsh
+++ 10_exports.zsh
@@ -8,7 +8,10 @@ export LS_COLORS='di=32:fi=0:ln=35:pi=5:so=5:bd=5:cd=5:or=31'
 export LSCOLORS="cxfxcxdxbxegedabagacad"
 export NODE_PATH='/usr/local/lib/node'
 export PAGER='less'
+export PIP_RESPECT_VIRTUALENV='true'
+export PIP_VIRTUALENV_BASE="$WORKON_HOME"
 export PYTHONSTARTUP="$HOME/.pythonrc"
 export READNULLCMD='cat'
 export SSH_ENV="$HOME/.ssh/environment"
 export UNAME="$(uname)"
+export WORKON_HOME="$HOME/.virtualenvs"
